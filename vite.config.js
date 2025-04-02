dee0f11b37327f6011cb8266be4e1b3ddb51e19e
麦耀强 <maiyaoqiang@zhongshitech.cn>
2025-04-02 10:44:45 +0800

    feat: 添加gzip压缩

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
+import viteCompression from 'vite-plugin-compression';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     uni(),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
   ],
   base: './',
   server: {
