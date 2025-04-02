728ac030572375bf5b77fc936447e459bad8c5f3
麦耀强 <maiyaoqiang@zhongshitech.cn>
2025-04-02 16:42:34 +0800

    feat: 优化密钥安全性

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import AV from 'leancloud-storage'
 AV.init({
-	appId: "2x5uatdDE4v2yJf0ngqF68iZ-gzGzoHsz",
-	appKey: "AJE1UYmZPvBNKKyzB3ou1VKb",
-	serverURL: "https://2x5uatdd.lc-cn-n1-shared.com",
+	appId: import.meta.env.VITE_APP_AV_APP_ID,
+	appKey: import.meta.env.VITE_APP_AV_APP_KEY,
+	serverURL: import.meta.env.VITE_APP_AV_SERVER_URL,
 });
 export function createApp() {
 	const app = createSSRApp(App);
